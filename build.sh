6272c3fd0d6f69727a3b8c9227eb6f82f7f46bc3
liyong31 <liyong460@gmail.com>
2023-05-26 21:20:54 +0800

    update bin

--- build.sh
+++ build.sh
@@ -10,12 +10,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_DIR=$SCRIPT_DIR/syntcomp-build/
 TP_DIR=$SCRIPT_DIR
 
-export LISA_TP=$SCRIPT_DIR/third_parties/
+export PATH=$PATH:$SCRIPT_DIR/binary
 
-echo -e "${GREEN}building cudd..${NC}"
-cd $LISA_TP/cudd-3.0.0/
-#./configure --enable-obj
-#make -j8
+export LISA_TP=$SCRIPT_DIR/third_parties/
 
 echo -e "${GREEN}building spot..${NC}"
 #cd $LISA_TP/spot-2.9.3/
@@ -23,8 +20,15 @@ echo -e "${GREEN}building spot..${NC}"
 #make -j8
 #make install
 
-echo -e "${GREEN}building modified aiger (it is not really needed)..${NC}"
-cd $LISA_TP/aiger-1.9.4/
+echo -e "${GREEN}building MONA..${NC}"
+#cd $LISA_TP/MONA/
+#./configure --prefix=$LISA_TP/mona-install-prefix/
+#make
+#make install-strip
+
+
+#echo -e "${GREEN}building modified aiger (it is not really needed)..${NC}"
+#cd $LISA_TP/aiger-1.9.4/
 #make
 
 echo -e "${GREEN}building lisa-syntcomp..${NC}"
@@ -37,12 +41,19 @@ make -j8 lisa-syntcomp-opt
 
 cd $SCRIPT_DIR
 
-rm -rf binary
-mkdir binary
-ln -s $BUILD_DIR/src/lisa-syntcomp-opt $SCRIPT_DIR/binary/lisa-syntcomp
+rm -rf bin
+mkdir bin
+#ln -s $BUILD_DIR/src/lisa-syntcomp-opt $SCRIPT_DIR/bin/lisa-syntcomp-opt
+cp $BUILD_DIR/src/lisa-syntcomp-opt $SCRIPT_DIR/bin/lisa-syntcomp-opt
+#ln -s $LISA_TP/mona-install-prefix/bin/mona $SCRIPT_DIR/bin/mona
+cp $LISA_TP/mona-install-prefix/bin/mona $SCRIPT_DIR/bin/mona
+cp $SCRIPT_DIR/syfco/syfco $SCRIPT_DIR/bin/syfco
+cp $SCRIPT_DIR/starexec_run_default.sh $SCRIPT_DIR/bin/starexec_run_default.sh
+cp $SCRIPT_DIR/starexec_run_real1.sh $SCRIPT_DIR/bin/starexec_run_real1.sh
+cp $SCRIPT_DIR/starexec_run_real2.sh $SCRIPT_DIR/bin/starexec_run_real2.sh
 
 echo
-echo -e "${GREEN}executable was created in binary/${NC}"
+echo -e "${GREEN}executable was created in bin/${NC}"
 
 # remove intermediate compilation files of spot to save space
 # (this _keeps_ the installed binaries and libs)
