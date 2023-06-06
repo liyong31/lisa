9d15690895cb4c9b9ed041e6549b6571665ff2e5
liyong31 <liyong460@gmail.com>
2023-06-06 11:23:43 +0800

    add more script

---
+++ bin/starexec_run_seqreal1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This configuration allows to check (un)realizability only
+# $1 contains the input filename (the name of the AIGER-file).
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PATH=$PATH:$SCRIPT_DIR
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_DIR/mona-install-prefix/lib
+
+COMMAND="./lisa-syntcomp-opt $1"
+$COMMAND
+res=$?
+if [[ $res == 10 ]]; then
+    echo "REALIZABLE"
+elif [[ $res == 20 ]]; then
+    echo "UNREALIZABLE"
+else
+    echo "UNKNOWN"
+fi
+exit $res
